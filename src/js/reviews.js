e6fe2a75b645d211934d111d8738a86fdef451ec
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-06-10 14:47:18 +0300

    Update reviews.js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -5,20 +5,28 @@ const form = refs.reviewForm;
 
 formShowBtn.addEventListener('click', () => {
   if (form.classList.contains('visually-hidden')) {
+    formShowBtn.setAttribute('disabled', 'true');
     form.classList.toggle('visually-hidden');
     form.classList.toggle('h0');
     form.classList.toggle('h486');
     form.classList.toggle('animate__fadeInDown');
     setTimeout(() => {
       form.classList.toggle('animate__fadeInDown');
+      setTimeout(() => {
+        formShowBtn.removeAttribute('disabled');
+      }, 1000);
     }, 1000);
   } else {
+    formShowBtn.setAttribute('disabled', 'true');
     form.classList.add('animate__fadeOutUp');
     form.classList.toggle('h0');
     form.classList.toggle('h486');
     setTimeout(() => {
       form.classList.toggle('visually-hidden');
       form.classList.remove('animate__fadeOutUp');
+      setTimeout(() => {
+        formShowBtn.removeAttribute('disabled');
+      }, 1000);
     }, 1000);
   }
 });
