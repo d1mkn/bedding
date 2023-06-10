508990ec68d643033471409246da6e32fbeb006b
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-06-10 18:05:39 +0300

    main page transition and animation

--- src/js/header.js
+++ src/js/header.js
@@ -1,29 +1,22 @@
 import { refs } from './refs/refs';
 import mobileMenu from './mobileMenu';
 
-function headerDropdown() {
-  const dropdownItems = refs.dropdownItems;
-
-  dropdownItems.forEach(item => {
-    const dropdownMenu = item.querySelector('.visually-hidden');
-
-    item.addEventListener('mouseenter', () => {
-      dropdownMenu.classList.remove('visually-hidden');
-    });
-
-    item.addEventListener('mouseleave', () => {
-      dropdownMenu.classList.add('visually-hidden');
-    });
-  });
-}
-headerDropdown();
-
 function headerSearchForm() {
   const searchBtn = refs.searchBtn;
   const searchForm = refs.searchForm;
 
   searchBtn.addEventListener('click', () => {
-    searchForm.classList.toggle('search__form--visible');
+    if (searchForm.classList.contains('visually-hidden')) {
+      searchForm.classList.toggle('animate__fadeIn');
+      searchForm.classList.toggle('visually-hidden');
+    } else {
+      searchForm.classList.toggle('animate__fadeIn');
+      searchForm.classList.toggle('animate__fadeOut');
+      setTimeout(() => {
+        searchForm.classList.toggle('visually-hidden');
+        searchForm.classList.toggle('animate__fadeOut');
+      }, 500);
+    }
   });
 }
 headerSearchForm();
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,6 +1,5 @@
 export const refs = {
   // header
-  dropdownItems: document.querySelectorAll('.js-dropdown'),
   searchBtn: document.querySelector('.js-search-btn'),
   searchForm: document.querySelector('.js-search-form'),
   // backdrop
